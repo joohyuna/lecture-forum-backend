4960c21cb134398efde36022d8a6fc73fe062373
mcm <joohyuna2@gmail.com>
2026-05-14 12:48:29 +0900

    cors 적용 + error message 변경

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import userRouter from "./routes/userRouter.ts";
+import cors from "cors";
 
 dotenv.config();
 
@@ -9,6 +10,11 @@ const app = express(); // 1. 이줄과
 const PORT = process.env.PORT || "8080";
 
 // express 앱에 기능을 확장할 때에는 app.use() 메서드 사용
+
+// 교차 출처 리소스 공유 (CORS)를 허용하는건 백엔드에서 증명하여 허용해야함
+// cors()만 사용하면 모든 프론트엔드 주소에 대해 허용 증명하는것
+// cors({origin: "조소"})를 통해 특정 주소에 대홰서만 허용 증명 할 수 있음
+app.use(cors());
 
 // express.json() : 요청(Request)의 본문(body)에서 JSON에 데이터를 객체로 변환 (파싱)하여 request.body에 저장
 app.use(express.json());
