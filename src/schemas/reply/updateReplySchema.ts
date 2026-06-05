397e88763ed27ceb3ed5b7dd8e6f1f313ed7d306
mcm <joohyuna2@gmail.com>
2026-06-05 12:51:02 +0900

    updateReplySchema.ts 오류 수정

--- src/schemas/reply/updateReplySchema.ts
+++ src/schemas/reply/updateReplySchema.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
-import { createReplySchema } from "./createReplySchema.ts";
 
 export const updateReplySchema = z.object({
     content: z.string().min(1, "댓글 내용은 필수값입니다."),
 });
 
-export type UpdateReplyInputType = z.infer<typeof createReplySchema>;
+export type UpdateReplyInputType = z.infer<typeof updateReplySchema>;
