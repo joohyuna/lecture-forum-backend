2c05c46f8139babc32b31873e3d09e29af903b10
mcm <joohyuna2@gmail.com>
2026-06-16 11:08:38 +0900

    userController.getMe 작성완료

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -10,6 +10,13 @@ import { withdrawUserSchema } from "../schemas/user/withdrawUserSchema.ts";
 
 const router = Router();
 
+//이 토큰이 정상적인지 확인을 하기 위한 요청이 들어오면,
+// 1. 해당 토근의 정보가 옳은지
+// 2. 이 사용자가 정상 사용자인지 => 서비스 쪽에서 user 테이블에서 정보를 잃어들임
+//                                => 요청을 한 사용자 정보를 뱉어주는 API를 만들면 되겠다.
+// 미들웨어로 끝나면 안됀다 next() 때문에
+router.get("/me", authenticate, userController.getMe);
+
 // /user/create 라고 post방식 요청이 도착하면 이 아래줄에 실행
 // /user/login
 router.post("/create", validate(createUserSchema), userController.createUser);
@@ -31,5 +38,7 @@ router.patch(
     validate(withdrawUserSchema),
     userController.updatePassword,
 );
+
+
 
 export default router;
